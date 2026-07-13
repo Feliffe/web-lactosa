ddd2b116ed143bd7dc8c1e097ee0c3138758c917
Iffe Ramírez <feliramirezjara@gmail.com>
2026-07-13 16:59:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -22,36 +22,3 @@ function desplegarInfo() {
         window.requestAnimationFrame(step);
     }
 }
-
-
-function copiarCorreo(event) {
-    event.preventDefault(); 
-    const correo = "contactolactosaintolerante@gmail.com";
-    
-    // Método de respaldo seguro para local y web
-    const tempInput = document.createElement("input");
-    tempInput.value = correo;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    tempInput.setSelectionRange(0, 99999); 
-    
-    try {
-        document.execCommand("copy");
-        mostrarNotificacion();
-    } catch (err) {
-        console.error("Error al copiar el correo: ", err);
-    }
-    
-    
-    document.body.removeChild(tempInput);
-}
-
-function mostrarNotificacion() {
-    const toast = document.getElementById('toast-notificacion');
-    
-    toast.classList.add('mostrar');
-    
-    setTimeout(() => {
-        toast.classList.remove('mostrar');
-    }, 3000);
-}
